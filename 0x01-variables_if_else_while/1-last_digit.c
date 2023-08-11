4e04aa64d2c8a0562882d0519f9bc298864202fc
Mirally <miralsandu@gmail.com>
2023-08-11 15:19:51 -0700

    Update betty line error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,27 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+* main - generate greater than and less than
+*
+* Return: always (0)
+*
+*/
 int main(void)
 {
 	int n;
-	int l;
+	int ld;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n % 10;
-	if (l > 5)
-{
-	printf("Last digit of %i is %i and is greater than 5\n", n, l);
-}
-	else if (l == 0)
-{
-	printf("Last digit of %i is %i and is 0\n", n, l);
-}
+	ld = n % 10;
+
+	if (ld > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5\n", n, ld);
+	}
+	else if (ld == 0)
+	{
+		printf("Last digit of %i is %i and is 0\n", n, ld);
+	}
 	else
-{
-	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
-}
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld);
+	}
 	return (0);
 }
