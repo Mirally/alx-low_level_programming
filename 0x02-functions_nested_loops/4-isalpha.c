1869913914f203839ff4d69c728e5a6821969a79
Mirally <miralsandu@gmail.com>
2023-08-16 19:30:59 -0700

    final update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,28 +1,10 @@
 #include "main.h"
 /**
- * _isalpha - return 1 when c is cap or low
- *
- * Description: we are usin _putchar
- *
+ * _isalpha - checks for character
  * @c: check characters
- *
- * Return: (0) always
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	char lower, upper;
-	int letter = '0';
-
-	for (lower = 'a'; lower <= 'z'; lower++)
-	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if (lower == c || upper == c)
-			{
-				letter = 1;
-			}
-
-		}
-	}
-	return (letter);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
