e86296cb0d70a10f961e752d0e615750371f5eb1
Mirally <miralsandu@gmail.com>
2023-08-16 17:25:57 -0700

    update error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,14 +6,14 @@
  *
  * @c: check characters
  *
- * Return: 1 if c is low or cap otherwise 0
+ * Return: (0) always
  */
 int _isalpha(int c)
 {
 	char lower, upper;
 	int letter = '0';
 
-	for  (lower = 'a'; lower <= 'z'; lower++)
+	for (lower = 'a'; lower <= 'z'; lower++)
 	{
 		for (upper = 'A'; upper <= 'Z'; upper++)
 		{
