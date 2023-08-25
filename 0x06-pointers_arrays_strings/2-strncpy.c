6aaa1022fb87cccbfe8f44cb276148ce07beed9a
Mirally <miralsandu@gmail.com>
2023-08-25 16:57:27 -0700

    Write a function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - Concatenate two given strings, src of size n
+ * @dest: string var
+ * @src: given string
+ * @n: size of src
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int z;
+
+	for (z = 0; z < n && src[z] != '\0'; z++)
+	{
+		dest[z] = src[z];
+	}
+	while (z < n)
+	{
+	dest[z] = '\0';
+	z++;
+	}
+	return (dest);
+}
